224a9ca1e026df34a14e494c35882314c00fc9cc
Nishant Shingate <62829800+Nishant0073@users.noreply.github.com>
2022-09-17 14:09:23 +0530

    Update merge_sort.cpp

--- Searching & Sorting/merge_sort.cpp
+++ Searching & Sorting/merge_sort.cpp
@@ -1,81 +1,61 @@
+/*
 
+The Merge Sort algorithm is a sorting algorithm that is based on the Divide and Conquer paradigm. 
+In this algorithm, the array is initially divided into two equal halves and then they are combined in a sorted manner.
 
-#include <bits/stdc++.h>
+Time Complexity: O(N log(N))
+Auxiliary Space: O(n)
+
+*/
+
+#include<bits/stdc++.h>
 using namespace std;
-void print(int arr[], int n)
-{
-        for (int i = 0; i < n; i++) cout << arr[i] << " ";
+
+
+void merge(int a[],int l,int r,int m){
+    int n1 = (m-l)+1;
+    int n2 = (r-m);
+
+    int a1[n1],a2[n2];
+    for(int i=0;i<n1;i++)
+        a1[i] = a[i+l];
+    for(int i=0;i<n2;i++)
+        a2[i] = a[i+m+1];
+
+    int i=0,j=0,k=l;
+    while(i<n1 && j<n2)
+    {
+        if(a1[i]<=a2[j])
+            a[k++] = a1[i++];
+        else
+            a[k++] = a2[j++];
+    }
+
+    while(i<n1)
+        a[k++] = a1[i++];
+
+    while(j<n2)
+        a[k++] = a2[j++];
 }
-
-void merge(int arr[], int l, int m, int r)
+void divide(int a[],int l,int r)
 {
-        int n1 = (m - l) + 1;
-        int n2 = (r - m);
-        int a1[n1], a2[n2];
-
-        for (int i = 0; i < n1; i++)
-        {
-                a1[i] = arr[l + i];
-        }
-        for (int i = 0; i < n2; i++)
-        {
-                a2[i] = arr[m + 1 + i];
-        }
-        int i = 0, j = 0, k = l;
-        while (i < n1 && j < n2)
-        {
-                if (a1[i] < a2[j])
-                {
-                        arr[k] = a1[i];
-                        k++, i++;
-                }
-                else
-                {
-                        arr[k] = a2[j];
-                        k++, j++;
-                }
-        }
-        while (i < n1)
-        {
-                arr[k] = a1[i];
-                i++, k++;
-        }
-        while (j < n2)
-        {
-                arr[k] = a2[j];
-                k++, j++;
-        }
+    if(l<r)
+    {
+       int m = (l+r)/2;
+       divide(a,l,m);
+       divide(a,m+1,r);
+       merge(a,l,r,m);
+    }
 }
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
 
-void merge_sort(int arr[], int l, int r)
-{
-        if (l < r)
-        {
-                int mid = (l + r) / 2;
-                merge_sort(arr, l, mid);
-                merge_sort(arr, mid + 1, r);
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    divide(a,0,n-1);
+    for(auto it:a)
+        cout<<it<<" ";
 
-                merge(arr, l, mid, r);
-        }
-}
-
-int main()
-{
-        int t;
-        cin >> t;
-        while (t--)
-        {
-                int n;
-                cin >> n;
-                int arr[n];
-
-                for (int i = 0; i < n; i++)
-                {
-                        cin >> arr[i];
-                }
-
-                merge_sort(arr, 0, n - 1);
-                print(arr, n);
-                cout<<endl;
-        }
 }
